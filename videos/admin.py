087e2ad5880dbc151771e7a80b12c5ca83ba9b79
torres <britomaximiliano83@gmail.com>
2021-07-20 12:46:11 +0700

    views config

--- videos/admin.py
+++ videos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserProfile
 
 # Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role', 'authorized',)
+
+admin.site.register(UserProfile, UserProfileAdmin)
